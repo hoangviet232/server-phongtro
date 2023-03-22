d5726185cdcb60c2ee6d248b3858ab8b8ae7f9da
hoanngviet232 <hoanngviet232@gmail.com>
2023-03-22 21:51:19 +0700

    commit

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ import connectDatabase from './src/config/connectDatabase'
 
 const app = express()
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: '*',
     methods: ["POST", 'GET', 'PUT', "DELETE"]
 }))
-app.use(express.json({limit:'10mb'}))
-app.use(express.urlencoded({ extended: true, limit:'10mb' }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 initRoutes(app)
 connectDatabase()
