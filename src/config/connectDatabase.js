c02ece8ca991bc7a6311fb42558c61066733a005
#viethoang <94123020+hoangviet232@users.noreply.github.com>
2023-10-23 11:45:49 +0700

    Update

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('phongtro123', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USENAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions:{
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+   
 });
 
 const connectDatabase = async () => {
